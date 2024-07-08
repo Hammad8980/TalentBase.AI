5b9963ba3c343cea2dcb2c8aa38aba4021e58e67
Hammad Mehmood <hammad.mehmood898@gmail.com>
2024-07-08 11:09:24 +0500

    changed navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,22 +3,25 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <nav className="App-nav">
-                <ul className='navbar ms-auto'>
-                    <a class="navbar-brand" href="#">TalentBase.Ai</a>
-                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    <Link className="nav-link" to="/enterprise">Enterprise</Link>
-                    <Link className="nav-link" to="/students">Students</Link>
-                    <Link className="nav-link" to="/about">About</Link>
-                    <Link className="nav-link" to="/blog">Blog</Link>
-                    <Link className="nav-link" to="/tnsfs">TNSFS</Link>
-                    <Link className="nav-link" to="/contact">Contact</Link>
-                </ul>
-                </nav>
-            </header>
+        <nav className="App-header navbar navbar-expand-lg navbar-dark ">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">TALENTBASE.AI</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            <Link className="nav-link" to="/enterprise">Enterprise</Link>
+            <Link className="nav-link" to="/students">Students</Link>
+            <Link className="nav-link" to="/about">About</Link>
+            <Link className="nav-link" to="/blog">Blog</Link>
+            <Link className="nav-link" to="/tnsfs">TNSFS</Link>
+            <Link className="nav-link" to="/contact">Contact</Link>
+          </div>
         </div>
+      </div>
+    </nav>
     )
 }
 
