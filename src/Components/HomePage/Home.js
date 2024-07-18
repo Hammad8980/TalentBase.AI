5c2ea67ea644b311ea9f4757e883411271b227d9
AbdulMoeezSultan <abdulmoeez0e@gmail.com>
2024-07-18 11:02:57 +0500

    changes

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -4,27 +4,32 @@ import Testimonials from './Testimonials/Testimonials';
 import Blog from '../BlogPage/Blog';
 import Newsletter from './NewsLetter/NewsLetter';
 import Footer from './Footer/Footer';
-import 'animate.css'
+import 'animate.css';
+import {useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate()
+  const moveToSignUp = () => {
+    navigate('/Signup');
+  };
   return (
     <>
       <main className="App-main">
-      <h1 className='animate__animated animate__fadeInDown'>Let us Build Brands<br></br> together shall we?</h1>
-      <div className="App-buttons animate__animated animate__fadeInDown">
-        <button className="start-button">GET STARTED</button>
-        <span>or</span>
-        <a href="/video">Watch video</a>
-      </div>
+        <h1 className='animate__animated animate__fadeInDown'>Let us Build Brands<br></br> together shall we?</h1>
+        <div className="App-buttons animate__animated animate__fadeInDown">
+          <button className="start-button" onClick={moveToSignUp  }>GET STARTED</button>
+          <span>or</span>
+          <a href="/video">Watch video</a>
+        </div>
       </main>
       <WhatWeDo />
       <Testimonials />
-      <Blog/>
+      <Blog />
       <Newsletter />
       <Footer />
     </>
-    
-    
+
+
   )
 }
 
--- src/Components/Missing.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Missing = () => {
-  return (
-    <main>Page not found</main>
-  )
-}
-
-export default Missing
