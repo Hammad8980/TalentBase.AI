bb31ce4110dac86483a2a6cad78f4eedffcf71d9
Hammad Mehmood <hammad.mehmood898@gmail.com>
2024-08-21 10:24:51 +0500

    Service page done

--- src/Components/ServicesPage/ServiceCard.jsx
+++ src/Components/ServicesPage/ServiceCard.jsx
@@ -3,12 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import styles from './ServiceCard.module.css';
 
 
-const ServiceCard = ({ title, description, longDescription, buttonText, imageSrc }) => {
+const ServiceCard = ({ title, description, longDescription, buttonText, imageSrc, onClick, isExpanded, onBackClick }) => {
+  const navigate = useNavigate();
+  function handleClick(event) {
+    event.stopPropagation();
+    navigate("/contact");
+  }
   const [isMobileView, setIsMobileView] = useState(window.innerWidth > 998);
-
+  const [isMobileViewExpanded, setIsMobileViewExpanded] = useState(window.innerWidth > 998);
   useEffect(() => {
     const handleResize = () => {
       setIsMobileView(window.innerWidth > 998);
+      setIsMobileViewExpanded(window.innerWidth > 998);
     };
 
     window.addEventListener('resize', handleResize);
@@ -18,8 +24,11 @@ const ServiceCard = ({ title, description, longDescription, buttonText, imageSrc
   }, []);
 
   return (
-    <section className={styles.serviceCard}>
-      {!isMobileView && (
+    <section 
+      className={`${styles.serviceCard} ${isExpanded ? styles.expanded : ''}`} 
+      onClick={isExpanded ? null : onClick}
+    > 
+      {(!isExpanded && !isMobileView || !isMobileViewExpanded)&& (
         <div className={styles.contentWrapper}>
           <img src={imageSrc} className={styles.imageWrapper} />
         </div>
@@ -27,15 +36,27 @@ const ServiceCard = ({ title, description, longDescription, buttonText, imageSrc
       <div className={styles.contentWrapper}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.description}>{description}</p>
-        <p className={styles.longDescription}>{longDescription}</p>
-        <button className={styles.ctaButton} >
-          <span>{buttonText}</span>
-          <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/98ec1962519cd7d1ca44565f2471918f29185cc10bc407da2bd1ab59fc55d4b4?placeholderIfAbsent=true&apiKey=168c23e5e966425abe0e4b54edde3b42" alt="" className={styles.buttonIcon} />
-        </button>
+        { isExpanded && <p className={styles.longDescription}>{longDescription}</p> }
+        {isExpanded ? (
+          <>
+            <button className={styles.ctaButton} onClick={handleClick}>
+            <span>{buttonText}</span>
+            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/98ec1962519cd7d1ca44565f2471918f29185cc10bc407da2bd1ab59fc55d4b4?placeholderIfAbsent=true&apiKey=168c23e5e966425abe0e4b54edde3b42" alt="" className={styles.buttonIcon} />
+            </button>
+            <button className={styles.backButton} onClick={onBackClick}>
+            Back
+            </button>
+          </>
+        ) : (
+          <button className={styles.ctaButton} onClick={handleClick}>
+            <span>{buttonText}</span>
+            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/98ec1962519cd7d1ca44565f2471918f29185cc10bc407da2bd1ab59fc55d4b4?placeholderIfAbsent=true&apiKey=168c23e5e966425abe0e4b54edde3b42" alt="" className={styles.buttonIcon} />
+          </button>
+        )}
       </div>
-      {isMobileView && (
+      {(isMobileView && isExpanded || isMobileViewExpanded) && (
         <div className={styles.contentWrapper}>
-          <img src={imageSrc} className={styles.imageWrapper} />
+          <img src={imageSrc} className={styles.imageWrapper} alt={title} />
         </div>
       )}
     </section>
--- src/Components/ServicesPage/Services.jsx
+++ src/Components/ServicesPage/Services.jsx
@@ -1,46 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ServiceCard from './ServiceCard';
 import styles from './Services.module.css';
 
 const servicesData = [
   {
-    title: "LEARNING PLATFORM:",
+    title: "LEARNING PLATFORM",
     description: "A degree program's learning stream is organized into various modules, each represented in three different levels, categorized as basic, intermediate, and advanced.",
     longDescription: "Upon completion of each module, students receive a badge of achievement. At the successful completion of the learning path, students are awarded a specialization in their chosen field, marking their proficiency and readiness for advanced professional opportunities.",
     buttonText: "Request for Services",
     imageSrc: "https://img.freepik.com/free-vector/e-learning-icons-flat_1284-3950.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724025600&semt=ais_hybrid"
   },
   {
-    title: "DIGITAL",
-    description: "loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg fnvefbubh bgjbiogjroib gboigoibj fveujir huihtrhg iuthiutruitr itrtriutrj jegiejrigre ifoioitjeogjetj etejiotrjoitjr ejwoiweoife ewfoirf hf",
-    longDescription: "loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg",
+    title: "CERTIFICATIONS AND WORKSHOPS",
+    description: "This category includes courses and workshops across different technical and business domains.",
+    longDescription: "Certificates are awarded aiming targeted skill and concentrated knowledge, designed for career enhancement in marketing domains.",
     buttonText: "Request for Services",
-    imageSrc: "./podcast2.jpg"
+    imageSrc: "https://www.shutterstock.com/image-photo/business-work-assessment-certificate-concept-600nw-2305273009.jpg"
   },
   {
-    title: "DIGITAL",
-    description: "loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg fnvefbubh bgjbiogjroib gboigoibj fveujir huihtrhg iuthiutruitr itrtriutrj jegiejrigre ifoioitjeogjetj etejiotrjoitjr ejwoiweoife ewfoirf hf",
-    longDescription: "loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg",
+    title: "BUSINESS DEVELOPMENT SERVICES",
+    description: "Marketing campaigns are organized to promote the products and services, through content writing and creative designs to engage and persuade the target audience.",
+    longDescription: "Different application softwares are created through Web development. Digital marketing strategies for advertisements drive website traffic and connect to the audience. The search engine optimization ranks the website to a higher rank.",
     buttonText: "Request for Services",
-    imageSrc: "./podcast2.jpg"
+    imageSrc: "./business-development-services.jpeg"
   },
   {
-    title: "DIGITAL",
-    description: "loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg fnvefbubh bgjbiogjroib gboigoibj fveujir huihtrhg iuthiutruitr itrtriutrj jegiejrigre ifoioitjeogjetj etejiotrjoitjr ejwoiweoife ewfoirf hf",
-    longDescription: "loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg loreum pas fhihrib jorgtjbgrb ibhfuihv hiugehih bugiubg",
+    title: "PODCAST SERVICES",
+    description: "Podcast services offer comprehensive assistance, requiring cameras and microphones for optimal recording.",
+    longDescription: "These services include media coverage through videos, reels, and pictures, as well as professional podcast recording. Our professional hosts are available for both formal and informal settings. Editing services are available to ensure high-quality content.",
     buttonText: "Request for Services",
     imageSrc: "./podcast2.jpg"
   }
 ];
 
-const Services = ( imageSrc ) => {
+const Services = () => {
+  const [expandedCardIndex, setExpandedCardIndex] = useState(null);
+  const handleCardClick = (index) => {
+    setExpandedCardIndex(index);
+  }
+  const handleBackClick = () => {
+    setExpandedCardIndex(null);
+  }
   return (
     <main className={styles.servicesContainer}>
       <div className={styles.servicesWrapper}>
-        <h1 className={styles.servicesTitle}>SERVICES</h1>
-        {servicesData.map((service, index) => (
-          <ServiceCard key={index} {...service} />
-        ))}
+        <h1 className={styles.servicesTitle}>OUR SERVICES</h1>
+        
+        {
+          expandedCardIndex === null ? (
+            servicesData.map((service, index) => (
+              <ServiceCard 
+              key={index}
+              {...service}
+              onClick={() => handleCardClick(index)}
+              isExpanded={false} />
+            ))
+          ) : (
+            <ServiceCard 
+            {...servicesData[expandedCardIndex]} 
+            onBackClick={handleBackClick} 
+            isExpanded={true}
+          />
+          )}
       </div>
     </main>
   );
